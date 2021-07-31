c359efef6ee8524ed727a29b09ca10510969c05e
Victor <vitocr_santos@hotmail.com>
2021-07-31 15:28:04 -0300

    Upload web version

--- Assets/Scripts/MovementSpeedType.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "MovementSpeedType", menuName = "Movement Speed Type")]
-public class MovementSpeedType : ScriptableObject
-{
-    public float speed = 1f;
-}
